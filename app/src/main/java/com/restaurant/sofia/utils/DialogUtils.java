a3571ccd7e17b69940bee0793d3eb5ada12d8b17
sofianidasofi <sofianida29@gmail.com>
2019-12-28 23:43:38 +0700

    Initial commit

---
+++ app/src/main/java/com/restaurant/sofia/utils/DialogUtils.java
@@ -0,0 +1,18 @@
+package com.restaurant.sofia.utils;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+
+public class DialogUtils {
+    public static ProgressDialog progressDialog;
+
+    public static void openDialog(Context context) {
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setMessage("Loading . . . ");
+        progressDialog.show();
+    }
+
+    public static void closeDialog() {
+        progressDialog.dismiss();
+    }
+}
